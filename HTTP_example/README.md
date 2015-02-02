c0c44d78437b36953089fddbe2349828063e6322
D. Herrendoerfer <herrendo@gmail.com>
2015-02-03 00:26:46 +0100

    Add an example web service sketch.

---
+++ HTTP_example/README.md
@@ -0,0 +1,15 @@
+HTTP_example
+============
+
+A small example to expose all digital and analog pins of an
+Arduino via a web service.
+
+Usage
+=====
+
+Copy this file to the TOI_firmware directory, compile and transfer.
+Run on a 328P based Arduino. Data pins for the ESP8266 are 10 and 11.
+
+Version History
+---------------
+2015/02/02 no version     - first upload
---
+++ HTTP_example/my_app_code.ino
@@ -0,0 +1,271 @@
+/*
+ * This file is part of TOI_firmware.
+ *
+ * Copyright (C) 2015  D.Herrendoerfer
+ *
+ *   TOI_firmware is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   TOI_firmware is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with TOI_firmware.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+//#define APP_DEBUG 1
+
+/* My-App specific init */
+void app_init()
+{
+
+}
+
+/* My-App specific URL handlers
+   Put your web-pages in here. */
+int myAppHandleURL(char* URL, int ch_id)
+{
+  if (strcmp(URL,"/index.html") == 0 || strcmp(URL,"/") == 0) {
+    /* index page */
+    page_index(ch_id, URL);
+    return 0;
+  }
+
+  if (strcmp(URL,"/analog.html") == 0) {
+    /* analog page */
+    page_analog(ch_id, URL);
+    return 0;
+  }
+
+  if (strcmp(URL,"/digital.html") == 0) {
+    /* digital page */
+    page_digital(ch_id, URL);
+    return 0;
+  }
+
+  if (strcmp(URL,"/output.html") == 0) {
+    /* temp page */
+    page_output(ch_id, URL);
+    return 0;
+  }
+
+  if (strstr(URL,"/oset.html?") == URL) {
+    /* time save page */
+    page_oset(ch_id, URL);
+    return 0;
+  }
+
+  return 1;
+}
+
+/* My-App specific shutdown code 
+   Note: a reboot or shutdown is in progress */
+void shutdown_event()
+{
+
+}
+
+/* My-App specific eeprom code 
+   Note: an eeprom-write was done */
+void eeprom_event()
+{
+
+}
+
+int tick()
+{
+  if(time % 500 == 0) {
+    // 500ms interval
+/*
+    for (int i=0; i<4; i++) {
+      int val = analogRead(A3);
+      temp_sensors[i] = (int)(u2totemp(val) *100);
+    }
+*/    
+  }
+}
+
+
+/*********************************************************************************************** 
+*  index.html 
+*
+*  Simple html page stored in progmem (flash) and sent via
+*  response_send_progmem()
+*/
+prog_char content_index[] PROGMEM =  "<HTML><BODY>index.html<hr>Arduino ESP8266 Web Server Sample<p>"
+                          "Available options:<br> "
+                          "Read analog inputs <a href=analog.html>analog.html</a><br>"
+                          "Read digital inputs <a href=digital.html>digital.html</a><br>"
+                          "Set digital outputs <a href=output.html>output.html</a><p>"
+                          "To change the Wifi configuration please use<br>"
+                          "the built-in config page <a href=arduino.html>arduino.html</a>"
+                          "</BODY></HTML>\r\n";
+  
+void page_index(int ch_id, char* URL) {
+  response_send_progmem(ch_id, content_index, sizeof(content_index));  
+}
+
+
+/*********************************************************************************************** 
+*  analog.html 
+*
+*  Return a web page via the stream send functions. Uses very little
+*  memory, but requires a lot of care.
+*/
+void page_analog(int ch_id, char* URL) {
+  
+  char buf[64];
+
+  if (stream_head(ch_id)) {
+    close_channel(ch_id);
+    return;  
+  }
+
+  sprintf(buf,"Analog:");
+  
+  for (int i=0; i<8; i++) {
+    if (i<7)
+      sprintf(buf, "%s%d,",buf,analogRead(i));
+    else
+      sprintf(buf, "%s%d",buf,analogRead(i));
+  }
+
+  sprintf(buf, "%s\r\n", buf);
+
+  stream_send(ch_id, buf);
+  close_channel(ch_id);
+  
+  return;  
+}
+
+/*********************************************************************************************** 
+*  digital.html 
+*
+*  Returns a web page via the response_send_simple() function.
+*  Requires some memory, but is fast and easy to use.
+*/
+void page_digital(int ch_id, char* URL) {
+  
+  char buf[64];
+
+  sprintf(buf,"Digital:");
+  
+  for (int i=2; i<10; i++) {
+    if (i<9)
+      sprintf(buf, "%s%d,",buf,digitalRead(i));
+    else
+      sprintf(buf, "%s%d",buf,digitalRead(i));
+  }
+
+  sprintf(buf, "%s\r\n", buf);
+
+  response_send_simple(ch_id, buf, strlen(buf));
+}
+
+/*********************************************************************************************** 
+*  ouput.html 
+*
+*  Send a generated page via the stream_send functions.
+*  Although pages can become very large uses very little
+*  resources.
+*/
+int outvalues[9] = {0,0,0,0,0,0,0,0,0};
+
+void page_output(int ch_id, char* URL) {
+  
+  char buf[128];
+
+  if (stream_head(ch_id)) {
+    close_channel(ch_id);
+    return;  
+  }
+  if (stream_send(ch_id,"<HTML><BODY>Outputs<hr><p>")) {
+    close_channel(ch_id);
+    return;  
+  }
+
+  for (int i=2; i<10; i++) {
+    sprintf(buf, "PIN: %d : value: %d  <a href=oset.html?ch=%d&st=1>ON</a>;<a href=oset.html?ch=%d&st=0>OFF</a><br>",i,outvalues[i],i,i);
+    if (stream_send(ch_id, buf)) {
+      close_channel(ch_id);
+      return;  
+    }
+  }
+
+  stream_send(ch_id,"<hr></BODY></HTML>\r\n");
+  close_channel(ch_id);
+  
+  return;  
+}
+
+/*********************************************************************************************** 
+*  oset.html 
+*
+*  Page with parameter handling. Parameters are stored in the URL string
+*  and can be retrieved, reponses are selected and send from progmem.
+*/
+
+prog_char content_oset[] PROGMEM = "<HTML><BODY>OUTPUT SET<hr><p><a href=output.html>Back</a></BODY></HTML>\r\n";
+prog_char content_oset_err[] PROGMEM = "<HTML><BODY>ERROR<hr><p><a href=output.html>Back</a></BODY></HTML>\r\n";
+
+void page_oset(int ch_id, char* URL)
+{
+  char* Params = strstr(URL,"?")+1;
+  int chan = 0;
+  int state = 0;
+
+#ifdef APP_DEBUG  
+  Serial.print("Params:" );
+  Serial.println(Params);
+#endif
+
+  char* r_chan=strstr(Params,"ch=")+3;
+  char* r_state=strstr(Params,"st=")+3;
+
+  if (strstr(r_chan,"&"))
+    *(strstr(r_chan,"&")) = 0;
+  if (strstr(r_state,"&"))
+    *(strstr(r_state,"&")) = 0;
+
+  sscanf(r_chan, "%d", &chan );
+  sscanf(r_state, "%d", &state );
+
+#ifdef APP_DEBUG  
+  Serial.print("Chan:" );
+  Serial.println(chan);
+  Serial.print("state:" );
+  Serial.println(state);
+#endif
+  
+  if (chan > 1 && chan < 10) {
+    if (state < 2) {
+      outvalues[chan]=state;
+      digitalWrite(chan,state);
+    } else {
+      if (state < 256) {
+        outvalues[chan]=state;
+        analogWrite(chan,state);
+      }
+      else {
+        response_send_progmem(ch_id, content_oset_err, sizeof(content_oset_err));
+        return;
+      }
+    }
+  }
+  else {
+    response_send_progmem(ch_id, content_oset_err, sizeof(content_oset_err));
+    return;
+  }
+
+  response_send_progmem(ch_id, content_oset, sizeof(content_oset));
+}
+
+/*********************************************************************************************** 
+*  Helpers 
+*/
+
+
